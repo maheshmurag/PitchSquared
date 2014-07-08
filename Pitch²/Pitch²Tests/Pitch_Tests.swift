d931525ec9cd93a5b1eb475037f935df96b58894
Alex Yeh <cluster5@dhcp-x233-055.mobile.uci.edu>
2014-07-08 15:10:53 -0700

    Create Pitch² Project

---
+++ Pitch²/Pitch²Tests/Pitch_Tests.swift
@@ -0,0 +1,35 @@
+//
+//  Pitch_Tests.swift
+//  Pitch²Tests
+//
+//  Created by Cluster 5 on 7/8/14.
+//  Copyright (c) 2014 Alex Yeh. All rights reserved.
+//
+
+import XCTest
+
+class Pitch_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
