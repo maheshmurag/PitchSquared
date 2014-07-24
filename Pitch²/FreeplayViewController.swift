b5a0e8c7686e16c5cc5111600fc08ec71e96ea4f
Alex Yeh <alexlyeh@gmail.com>
2014-07-24 15:09:10 -0700

    Fix Recording

--- Pitch²/FreeplayViewController.swift
+++ Pitch²/FreeplayViewController.swift
@@ -30,7 +30,7 @@ class FreeplayViewController: UIViewController {
     var scale : Int = 0
     var prevRec: Bool;
     var hasRec : Bool = false;
-    
+    var rec: Bool = false;
     var freqList : Float[] = [110.0,116.54,123.47,130.81,138.59,146.83,155.56,164.81,174.61,185.00,196.00,207.65,220.0,233.08,246.94,261.63,277.18,293.66,311.13,329.63,349.23,369.99,392.63,415.30,440.00,466.16,493.88,523.25,554.37,587.33,622.25,659.25,698.46,739.99,783.99,830.61,880.0,932.33,987.77,1046.50,1108.73,1174.66,1244.51,1318.51,1396.91,1479.98,1567.98,1661.22,1760.00];
     
     var freqListNote : Dictionary<Float, String> = [110.0:"A2",116.54:"A2S",123.47:"B2",130.81:"C3",138.59:"C3S",146.83:"D3",155.56:"D3S",164.81:"E3",174.61:"F3",185.00:"F3S",196.00:"G3",207.65:"G3S",220.0:"A3",233.08:"A3S",246.94:"B3",261.63:"C4",277.18:"C4S",293.66:"D4",311.13:"D4S",329.63:"E4",349.23:"F4",369.99:"F4S",392.63:"G4",415.30:"G4S",440.00:"A4",466.16:"A4S",493.88:"B4",523.25:"C5",554.37:"C5S",587.33:"D5",622.25:"D5S",659.25:"E5",698.46:"F5",739.99:"F5S",783.99:"G5",830.61:"G5S",880.0:"A5",932.33:"A5S",987.77:"B5",1046.50:"C6",1108.73:"C6S",1174.66:"D6",1244.51:"D6S",1318.51:"E6",1396.91:"F6",1479.98:"F6S",1567.98:"G6",1661.22:"G6S",1760.00:"A6"];
@@ -168,6 +168,10 @@ class FreeplayViewController: UIViewController {
 //        println(paths);
 //        PdBase.sendMessage(paths, withArguments: nil, toReceiver: "fileLoc");
         
+        var paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        var samplePath = paths.stringByAppendingPathComponent("recording.wav")
+        PdBase.sendMessage(samplePath, withArguments: nil, toReceiver: "filename")
+        
     }
     
     func diffMill() -> Float{
@@ -191,13 +195,14 @@ class FreeplayViewController: UIViewController {
         }
         
         if (recordOn) {
-            PdBase.sendFloat(20000, toReceiver:"recLen")
-            PdBase.sendBangToReceiver("rec")
+            PdBase.sendBangToReceiver("start")
+//            PdBase.sendFloat(20000, toReceiver:"recLen")
+//            PdBase.sendBangToReceiver("rec")
         }
             
         else if (recordOn != true && prevRec == true) {
-            PdBase.sendFloat(diffMill() * 44100.0, toReceiver: "resVal")
-            println(diffMill() * 44100)
+//            PdBase.sendFloat(diffMill() * 44100.0, toReceiver: "resVal")
+//            println(diffMill() * 44100)
             PdBase.sendBangToReceiver("stop")
             hasRec = true;
         }
@@ -206,13 +211,11 @@ class FreeplayViewController: UIViewController {
     }
     
     @IBAction func playbackAction(sender: AnyObject) {
-//        var paths = "/Users/cluster5/hi"
-//        paths = paths.stringByAppendingString("/hi");
-//        println(paths);
-//        PdBase.sendMessage(paths, withArguments: nil, toReceiver: "fileLoc");
-        if(hasRec){
-            PdBase.sendBangToReceiver("replay")
-        }
+        var paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        var samplePath = paths.stringByAppendingPathComponent("recording.wav")
+        println(samplePath)
+        PdBase.sendMessage(samplePath, withArguments: nil, toReceiver: "play")
+
     }
     
     override func didReceiveMemoryWarning() {
